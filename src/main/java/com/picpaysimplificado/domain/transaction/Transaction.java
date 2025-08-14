4919c673b3fa7a3f1fe58b899038bd318cc95d7d
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2025-08-14 18:46:20 -0300

    Criando as Transações
    Criação da transações no domain

--- src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -1,4 +1,31 @@
 package com.picpaysimplificado.domain.transaction;
 
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private BigDecimal amount;
+    @JoinColumn(name = "sander_id")
+    @ManyToOne
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
 }
