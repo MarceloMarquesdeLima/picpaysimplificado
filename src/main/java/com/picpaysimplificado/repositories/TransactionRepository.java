ca08f2c85078e6e71347ee6df641c543e9964034
Marcelo Marques de Lima <111276703+MarceloMarquesdeLima@users.noreply.github.com>
2025-08-15 00:45:34 -0300

    Update TransactionRepository.java
    Criando o repository transaction

--- src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
@@ -3,5 +3,4 @@ package com.picpaysimplificado.repositories;
 import com.picpaysimplificado.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
+public interface TransactionRepository extends JpaRepository<Transaction, Long> { }
